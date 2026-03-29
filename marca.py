230cd8d718211353c92d2ae7ff7184e9dcac1d63
ffernandosilvaa <lsffernandosilva@gmail.com>
2026-03-29 13:59:23 -0300

    Refactor habit management functions and file name

--- marca.py
+++ marca.py
@@ -1,19 +1,31 @@
 import json, datetime, sys, math
 
-file = 'habitos.json'
-habit_id = int(sys.argv[1]) if len(sys.argv) > 1 else 1
+file = 'habitats.json' # Ajustado para o nome que vi no seu print
 
-with open(file, 'r+') as f:
-    data = json.load(f)
+def carregar():
+    with open(file, 'r') as f: return json.load(f)
+
+def salvar(data):
+    with open(file, 'w') as f: json.dump(data, f, indent=2)
+
+def checkin(habit_id):
+    data = carregar()
     today = str(datetime.date.today())
-    
     for h in data['habits']:
-        if h['id'] == habit_id and today not in h['history']:
+        if h['id'] == int(habit_id) and today not in h['history']:
             h['history'].append(today)
             data['user']['xp'] += h['xp_yield']
-            # Formula: Level 1 = 0 XP, Level 2 = 100 XP, Level 3 = 400 XP...
             data['user']['level'] = math.floor(math.sqrt(data['user']['xp'] / 25)) + 1
-            
-    f.seek(0)
-    json.dump(data, f, indent=2)
-    f.truncate()
+    salvar(data)
+
+def add_habito(nome, xp):
+    data = carregar()
+    novo_id = max([h['id'] for h in data['habits']]) + 1 if data['habits'] else 1
+    data['habits'].append({"id": novo_id, "name": nome, "xp_yield": int(xp), "history": []})
+    salvar(data)
+
+if __name__ == "__main__":
+    if sys.argv[1] == "add":
+        add_habito(sys.argv[2], sys.argv[3])
+    else:
+        checkin(sys.argv[1])
